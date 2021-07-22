84d41702d341e63a6e0de6cb4330a5cbff87e7ed
DaniellaMinyo <daniellaminyo99@gmail.com>
2021-07-22 09:10:31 +0000

    style footer

--- app.py
+++ app.py
@@ -82,12 +82,15 @@ def login():
             {"username": request.form.get("username").lower()})
 
         if existing_user:
+            # make a variable for checking the password
+            password_match = check_password_hash(
+                existing_user["password"],
+                request.form.get("password")
+            )
             # ensure hashed password matches user input
-            if check_password_hash(
-                existing_user["password"], request.form.get("password")):
+            if password_match:
                 session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(
-                    request.form.get("username")))
+                flash("Welcome, {}".format(request.form.get("username")))
                 return redirect(url_for(
                     "profile", username=session["user"]))
             else:
